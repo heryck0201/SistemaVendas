3d36427b3a3fa2839717b18b9900c9a90fff9bab
heryck0201 <oliveiratarso06@gmail.com>
2022-04-25 16:26:28 -0300

    Cadastro de cliente I

---
+++ SistemaVendas/Controllers/CategoriaController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendas.DAL;
+using SistemaVendas.Entidades;
+using SistemaVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        protected ApplicationDbContext _applicationDbContext;
+        public CategoriaController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = _applicationDbContext.Categoria.ToList();
+            //dispose é para liberar memória
+            _applicationDbContext.Dispose();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            CategoriaViewModel viewModel = new CategoriaViewModel();
+
+            if (id != null)
+            {
+                var entidade = _applicationDbContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entidade.Codigo;
+                viewModel.Descricao = entidade.Descricao;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(CategoriaViewModel categoriaViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Categoria objCategoria = new Categoria()
+                {
+                    Codigo = categoriaViewModel.Codigo ,
+                    Descricao = categoriaViewModel.Descricao
+                };
+                if (categoriaViewModel.Descricao == null)
+                {
+                    _applicationDbContext.Categoria.Add(objCategoria);
+                }
+                else
+                {
+                    _applicationDbContext.Entry(objCategoria).State = EntityState.Modified;
+                }
+                _applicationDbContext.SaveChanges();
+            }
+
+            else
+            {
+                return View(categoriaViewModel);
+            }
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var ent = new Categoria()
+            {
+                Codigo = id
+            }; 
+            _applicationDbContext.Attach(ent);
+            _applicationDbContext.Remove(ent);
+            _applicationDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SistemaVendas/Models/CategoriaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Models
+{
+    public class CategoriaViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da Categoria")]
+        public string  Descricao { get; set; }
+    }
+}
